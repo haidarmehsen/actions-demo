5d7d01d5ade2a4b1df5159818c4ac86b68598cb7
Haidar <haidarrmehsen@gmail.com>
2024-07-22 16:11:28 +0300

    a123

---
+++ .github/scripts/demo_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# parse the name and age of the user from the command that ran the script
+name=$1
+age=$2
+
+# print the name and age of the user
+echo "Hello $name, you are $age years old!"
